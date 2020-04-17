332c14b8efe52b096327f044a505dce43bdd351e
standaert <w.standaert@hotmail.com>
2020-04-18 00:38:53 +0200

    swipeable layout

--- App.tsx
+++ App.tsx
@@ -1,19 +1,56 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up to start working on your app!</Text>
-    </View>
-  );
-}
+import Swiper from "react-native-swiper";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10,
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5,
   },
 });
+
+function PlayersMap() {
+  return (
+    <View>
+      <Text> Player map </Text>
+    </View>
+  );
+}
+
+function GamesOverview() {
+  return (
+    <View>
+      <Text> Games Overview </Text>
+    </View>
+  );
+}
+
+function PlayerStats() {
+  return (
+    <View>
+      <Text> Your stats </Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <Swiper showsPagination={false} loop={false}>
+      <GamesOverview />
+      <PlayersMap />
+      <PlayerStats />
+    </Swiper>
+  );
+}
