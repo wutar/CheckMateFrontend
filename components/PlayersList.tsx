12a45197eaa8003d683db1901aec145ec4c4fd68
standaert <w.standaert@hotmail.com>
2020-06-03 23:07:15 +0200

    some playerlist stylings

--- components/PlayersList.tsx
+++ components/PlayersList.tsx
@@ -1,18 +1,48 @@
-import { ListView, FlatList, Text } from "react-native";
+import { ListView, FlatList, StyleSheet, View } from "react-native";
+import { Text } from "react-native-elements";
 import { LocationContext } from "../Contexts/LocationContext";
-import React, { useContext } from "react";
-
+import React, { useContext, Fragment } from "react";
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#222222",
+  },
+  h1: {
+    fontSize: 20,
+    color: "#ffffff",
+    marginBottom: 20,
+    marginLeft: 20,
+  },
+  user: {
+    borderStyle: "solid",
+    borderColor: "white",
+    borderWidth: 2,
+    paddingTop: 15,
+    paddingBottom: 10,
+    paddingLeft: 15,
+    fontSize: 25,
+    color: "#ffffff",
+  },
+  distance: {},
+});
 export default function PlayersList() {
   const location = useContext(LocationContext);
   const getUsers = (): Array<JSX.Element> => {
     const userListItems = location.nearUsers.map((user) => {
       return (
-        <Text key={user.name}>
-          {user.name} {user.hitMetadata.distance}{" "}
+        <Text key={user.name} style={styles.user}>
+          {user.name} {user.hitMetadata.distance.toFixed(3)} km
         </Text>
       );
     });
     return userListItems;
   };
-  return <>{getUsers()}</>;
+  return (
+    <View style={styles.container}>
+      <Text h1 style={styles.h1}>
+        Nearby users
+      </Text>
+      {getUsers()}
+    </View>
+  );
 }
