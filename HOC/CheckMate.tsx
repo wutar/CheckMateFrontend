7820a3773fa7b24f3e7de2ad2eda3cc5676a3047
standaert <w.standaert@hotmail.com>
2020-06-04 11:49:34 +0200

    neat nav icon style

--- HOC/CheckMate.tsx
+++ HOC/CheckMate.tsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState, useRef, useContext } from "react";
-import { Text, View, Button, StyleSheet, Dimensions } from "react-native";
+import {
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from "react-native";
 import { ButtonGroup, ElementObject, Input } from "react-native-elements";
 import Swiper from "react-native-swiper";
 import PlayersMap from "../components/PlayersMap";
@@ -7,6 +14,9 @@ import { AuthContext, AuthContextProps } from "../Contexts/AuthContext";
 import AuthorizationApp from "../components/auth/AuthorizationApp";
 import PlayersList from "../components/PlayersList";
 import PlayerStats from "../components/PlayerStats";
+const opponents = require("./img/face-to-face.png");
+const map = require("./img/treasure-map.png");
+const profile = require("./img/ninja-heroic-stance.png");
 
 const windowHeight = Dimensions.get("window").height - 240;
 const styles = StyleSheet.create({
@@ -16,20 +26,50 @@ const styles = StyleSheet.create({
     height: windowHeight,
   },
   floatingButtons: {
+    backgroundColor: "#ffffff00",
+    height: 60,
+    width: "100%",
+    borderWidth: 0,
     alignItems: "center",
     justifyContent: "center",
     position: "absolute",
-    bottom: 10,
-    left: 10,
-    right: 10,
+    bottom: 30,
+    left: 5,
+    right: 5,
     borderRadius: 10,
+  },
+  navIcon: {
+    borderWidth: 0,
+    height: 60,
+    width: 60,
+  },
+  button: {
+    borderWidth: 0,
+    backgroundColor: "#ffffff00",
+    height: 60,
+    width: 60,
+    marginHorizontal: 20,
   },
 });
 
 export default function CheckMate() {
   const [selectedIndex, setSelectedIndex] = useState(1);
   const swiper = useRef<Swiper>(null);
-  const buttons = ["ToGames", "ToMap", "ToStats"];
+  const toPlayersListButton = (): JSX.Element => (
+    <Image source={opponents} style={styles.navIcon} />
+  );
+  const toPlayersMapButton = (): JSX.Element => (
+    <Image source={map} style={styles.navIcon} />
+  );
+  const toPlayerStatsButton = (): JSX.Element => (
+    <Image source={profile} style={styles.navIcon} />
+  );
+
+  const buttons = [
+    { element: toPlayersListButton },
+    { element: toPlayersMapButton },
+    { element: toPlayerStatsButton },
+  ];
   const updateIndex = (index) => {
     swiper.current?.scrollTo(index);
   };
@@ -37,6 +77,8 @@ export default function CheckMate() {
     return (
       <ButtonGroup
         containerStyle={styles.floatingButtons}
+        innerBorderStyle={{ width: 0, color: "#ffffffff" }}
+        buttonStyle={styles.button}
         buttons={buttons}
         onPress={(index) => {
           updateIndex(index);
